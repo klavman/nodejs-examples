fb28144f57c062e44c519b9b782fa7769983fdcf
Fco. Javier Clavero <jc.klavman76@gmail.com>
2017-05-07 15:57:54 +0200

    first example

---
+++ 00-basics/00-process.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/*
+
+https://nodejs.org/docs/latest-v5.x/api/process.html
+
+*/
+
+function processInfo()
+{
+	console.log("         PROCESS  NODE.JS         ");
+	console.log("process id: " + process.pid);
+	console.log("title: " + process.title);
+	console.log("node directory: " + process.execPath);
+	console.log("current directory: " + process.cwd());
+	console.log("node version: " + process.version);
+	console.log("dependecies: " + process.versions);
+	console.log("Plataform: " + process.platform);
+	console.log("architecture: " + process.arch);
+	console.log("node uptime: " + process.uptime());
+	console.log("argv: " + process.argv);
+}
+
+processInfo();
