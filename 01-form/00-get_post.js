411787b9988812b867bf1deaec5a34b71fdf8cee
Fco. Javier Clavero <jc.klavman76@gmail.com>
2017-05-30 16:41:26 +0200

    add example 01-form > 00-get_post

---
+++ 01-form/00-get_post.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const form = require('fs').readFileSync("statics/form.html");
+const querystring = require('querystring');
+const util = require('util');
+
+var datastring = '';
+
+
+http.createServer((request, response) => {
+
+	const headers = {'Content-type':'text/html'};
+
+	if (request.method === 'GET') {
+
+		response.writeHead(200, headers);
+		response.end(form);
+
+	}
+
+	if (request.method === 'POST') {
+
+		request
+
+			.on('data', (data) => {
+				datastring += data;
+			})
+
+			.on('end', () => {
+
+				const dataObject = querystring.parse(datastring);
+				const dataJSON = util.inspect(dataObject);
+
+				console.log(`data send POST (string): ${datastring}`);
+				console.log(dataObject);
+				
+				console.log(`data send POST (json): ${dataJSON}`);
+
+				response.end('form send');
+			});
+
+	}
+
+}).listen(3000);
---
+++ 01-form/es5/00-get_post.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var http = require('http');
+var form = require('fs').readFileSync("statics/form.html");
+var querystring = require('querystring');
+var util = require('util');
+
+var datastring = '';
+
+http.createServer(function (request, response) {
+
+	var headers = { 'Content-type': 'text/html' };
+
+	if (request.method === 'GET') {
+
+		response.writeHead(200, headers);
+		response.end(form);
+	}
+
+	if (request.method === 'POST') {
+
+		request.on('data', function (data) {
+			datastring += data;
+		}).on('end', function () {
+
+			var dataObject = querystring.parse(datastring);
+			var dataJSON = util.inspect(dataObject);
+
+			console.log('data send POST (string): ' + datastring);
+			console.log(dataObject);
+
+			console.log('data send POST (json): ' + dataJSON);
+
+			response.end('form send');
+		});
+	}
+}).listen(3000);
